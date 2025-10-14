e60a18fd77682842f3968c253ed7da789343ad3c
Maxime Folschette <Maxime.Folschette@centralelille.fr>
2025-10-14 14:03:59 +0200

    suggest V3 avec couche information effective

--- suggest/data.php
+++ suggest/data.php
@@ -6,14 +6,18 @@ if (isset($_GET["debutNom"]))
 	// On va ouvrir un fichier et afficher les lignes 
 	// où le prénom ou le nom contient ce texte
 
-	$tabLignes = file("le2_toupdate.txt");
+	$tabLignes = file("LE2_2025_2026.csv");
 	foreach ($tabLignes as $ligne)
 	{
 		// EXO1 : effectuer une recherche sur nom ou prénom 
-		if (preg_match("/^.*:(" . $cherche . ".*)$/i",$ligne,$tabResultats))
+		if (preg_match("/(^(.*):(" . $cherche . ".*):.*$)|" .
+		                "(^(" . $cherche . ".*):.*:.*$)/i",
+		      $ligne,$tabResultats))
 		{
 			// EXO2 afficher nom ET prénom 
-			echo $tabResultats[1]; 
+			$tabResultats = explode(":", $ligne);
+			echo "<div>" . $tabResultats[0] . " " .
+			  $tabResultats[1] . "</div>";
 		}
 	}
 
