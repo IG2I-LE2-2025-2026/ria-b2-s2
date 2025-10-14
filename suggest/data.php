7e5d121dc6effc6705f7d3813030d618108bec75
Maxime Folschette <Maxime.Folschette@centralelille.fr>
2025-10-14 14:35:46 +0200

    suggest V4 : envoi de JSON

--- suggest/data.php
+++ suggest/data.php
@@ -1,27 +1,38 @@
 <?php
 if (isset($_GET["debutNom"])) 
 {
-	$cherche = $_GET["debutNom"]; 
-	
-	// On va ouvrir un fichier et afficher les lignes 
-	// où le prénom ou le nom contient ce texte
+  $cherche = html_entity_decode($_GET["debutNom"],
+    ENT_QUOTES | ENT_SUBSTITUTE | ENT_HTML401,
+    "UTF-8"); 
+  // FIXME : Problème de décocdage des caractères accentués
+  
+  // On va ouvrir un fichier et afficher les lignes 
+  // où le prénom ou le nom contient ce texte
 
-	$tabLignes = file("LE2_2025_2026.csv");
-	foreach ($tabLignes as $ligne)
-	{
-		// EXO1 : effectuer une recherche sur nom ou prénom 
-		if (preg_match("/(^(.*):(" . $cherche . ".*):.*$)|" .
-		                "(^(" . $cherche . ".*):.*:.*$)/i",
-		      $ligne,$tabResultats))
-		{
-			// EXO2 afficher nom ET prénom 
-			$tabResultats = explode(":", $ligne);
-			echo "<div>" . $tabResultats[0] . " " .
-			  $tabResultats[1] . "</div>";
-		}
-	}
-
-	die("");
+  $tabLignes = file("LE2_2025_2026.csv");
+  $res = array();
+  foreach ($tabLignes as $ligne)
+  {
+    // EXO1 : effectuer une recherche sur nom ou prénom 
+    if (preg_match("/(^(.*):(" . $cherche . ".*):.*$)|" .
+                    "(^(" . $cherche . ".*):.*:.*$)/i",
+          $ligne,$tabResultats))
+    {
+      // EXO2 afficher nom ET prénom 
+      $tabResultats = explode(":", rtrim($ligne));
+      /*
+      echo "<div>" . $tabResultats[0] . " " .
+        $tabResultats[1] . "</div>";
+      */
+      $res[] = array("nom" => $tabResultats[0],
+                     "prenom" => $tabResultats[1],
+                     "id" => $tabResultats[2]);
+    }
+  }
+  
+  echo json_encode($res);
+  
+  die("");
 }
 
 ?>
